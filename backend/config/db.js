f92beb04252ab198c4b3ffcc6027d7b50e3ef2ac
SehajbirSingh <singhsehajbirs1906@gmail.com>
2024-11-24 01:12:57 +0530

    Data Base Connected by Prince

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 export const connectDb =  async ()=>{
-    await mongoose.connect('mongodb+srv://singhsehajbir1906:sehajbir1906@cluster0.xy1mp.mongodb.net/food-del').then(()=>console.log("!!!    Db Connected Suceessfully    |||"));
+    await mongoose.connect('mongodb+srv://princekumar8574:root@cluster0.rmipx.mongodb.net/Snap-bite').then(()=>console.log("!!!    Db Connected Suceessfully  "));
 
-}
+}  
+
